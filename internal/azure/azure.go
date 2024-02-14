da6668eb3a62eab5db0e215449acfe11462116a8
Brendan Thompson <github@blt.is>
2024-02-14 11:49:24 +1100

    refactor: move to internal, add license
    Signed-off-by: Brendan Thompson <github@blt.is>

--- internal/azure/azure.go
+++ internal/azure/azure.go
@@ -1,9 +1,19 @@
+/*
+Copyright Brendan Thompson
+
+Licensed under the PolyForm Internal Use License, Version 1.0.0 (the "License");
+you may not use this file except in compliance with the License.
+A copy of the License may be obtained at
+
+https://polyformproject.org/licenses/internal-use/1.0.0/
+*/
+
 package azure
 
 import (
 	"context"
-	"instancescheduler/patchwindow"
-	"instancescheduler/schedule"
+	"instancescheduler/internal/patchwindow"
+	"instancescheduler/internal/schedule"
 	"strings"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
--- main.go
+++ main.go
@@ -1,10 +1,20 @@
+/*
+Copyright Brendan Thompson
+
+Licensed under the PolyForm Internal Use License, Version 1.0.0 (the "License");
+you may not use this file except in compliance with the License.
+A copy of the License may be obtained at
+
+https://polyformproject.org/licenses/internal-use/1.0.0/
+*/
+
 package main
 
 import (
 	"flag"
 	"os"
 
-	"instancescheduler/azure"
+	"instancescheduler/internal/azure"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
